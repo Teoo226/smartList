a5d44bffdda008c57b93129877df92e7d3026c5c
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-05-23 22:17:43 -0300

    alterado o comportamento de scroll, apatir do clique em 'registre-se', para um scroll mais suave

--- smart-list-app/src/componentes/Navbar.js
+++ smart-list-app/src/componentes/Navbar.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import '../css/Navbar.css';
 
 export default class Navbar extends Component{
+  constructor(){
+    super();
+    this.state = {
+
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(evento){
+    let elementPosition = document.querySelector("#sectionRegistro").offsetTop - 80;
+    evento.preventDefault();
+
+    window.scroll({
+      top: elementPosition,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
   render(){
     return (
       <nav className="navbar">
@@ -9,8 +29,8 @@ export default class Navbar extends Component{
           <span>SmartList</span>
         </div>
         <ul className="login-register">
-          <li><a href="#sectionRegistro">Registre-se</a></li>
-          <li><a href="#">Login</a></li>
+          <li><a href="#sectionRegistro" onClick={this.scrollToSection}>Registre-se</a></li>
+          <li><a href="">Login</a></li>
         </ul>
       </nav>
     );
